e8bd9b55fff68b33adee64ecbd654c93d74aa570
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-06 15:58:30 -0400

    add default props to game container

--- src/Game.js
+++ src/Game.js
@@ -3,8 +3,16 @@ import Square from "./Square";
 import "./Game.css";
 
 class Game extends Component {
+  static defaultProps = {
+    grid: { x: 7, y: 7 },
+  };
+
   render() {
-    return <Square />;
+    return (
+      <div className="Game">
+        <Square />
+      </div>
+    );
   }
 }
 
